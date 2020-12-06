5bb72aef6f6f141209bcc6ad69aecd748ddadd3b
youdeliang <1048726003@qq.com>
2020-12-07 00:05:21 +0800

    feat: add compress

--- src/config/defaultConfig.js
+++ src/config/defaultConfig.js
@@ -3,5 +3,6 @@ const process = require('process');
 module.exports = {
   root: process.cwd(),
   hostname: '127.0.0.1',
-  port: '9527'
+  port: '9527',
+  compress: /\.(html|js|css|md)/
 }
--- src/helper/route.js
+++ src/helper/route.js
@@ -4,20 +4,36 @@ const promisify = require('util').promisify
 const stat = promisify(fs.stat)
 const Handlebars = require('handlebars')
 const readdir = promisify(fs.readdir)
+const mime = require('./mime')
 
 // eslint-disable-next-line no-undef
 const tplPath = path.join(__dirname, '../template/dir.tpl')
 const source = fs.readFileSync(tplPath)
 const template = Handlebars.compile(source.toString())
 const conf = require('../config/defaultConfig')
+const compress = require('./compress')
+const range = require('./range')
 
 module.exports = async function (req, res, filePath) {
   try {
     const stats = await stat(filePath)
     if (stats.isFile()) {
       res.statusCode = 200
-      res.setHeader('Content-Type', 'text/plain')
-      fs.createReadStream(filePath).pipe(res)
+      const contentType = mime(filePath)
+      res.setHeader('Content-Type', contentType)
+      let rs
+      const { code, start, end } = range(stats.size, req, res)
+      if (code === 200) {
+        res.statusCode = 200
+        rs = fs.createReadStream(filePath)
+      } else {
+        res.statusCode = 206
+        rs = fs.createReadStream(filePath, { start, end })
+      }
+      if (filePath.match(conf.compress)) {
+        rs = compress(rs, req, res)
+      }
+      rs.pipe(res)
     } else if (stats.isDirectory()) {
       const files = await readdir(filePath)
       res.statusCode = 200
